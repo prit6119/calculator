22f40a9ceb580aaed8154c85196d49106bb3ced8
prit6119 <90923545+prit6119@users.noreply.github.com>
2024-01-12 12:01:20 +0530

    Add files via upload
    all operation +, -, * , /

---
+++ calculator/calc/operations.go
@@ -0,0 +1,25 @@
+package calc
+
+import "fmt"
+
+func Add (a, b float64)float64{
+	return a + b
+}
+
+func Subtract(a, b float64)float64{
+	return a - b
+}
+
+func Multiply(a, b float64)float64{
+	return a * b
+}
+
+func Divide(a, b float64)(float64, error){
+	
+	if b== 0{
+	return 0, fmt.Errorf("division by zero")
+	}
+	
+	return a / b, nil
+	
+}
